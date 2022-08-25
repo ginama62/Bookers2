30bfd96cfb45a6fe8e5939ae55fe2359ab3291fc
ginama62 <107463338+ginama62@users.noreply.github.com>
2022-08-25 11:46:25 +0000

    [Add] 課題手順チェックシート15

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  resources :books, only: [:new, :create, :index, :show]
+  get 'books/edit'
+  root to: "homes#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
