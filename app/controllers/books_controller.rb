c5ae64db1156c72ba4deb6bd710af069278c7817
ginama62 <107463338+ginama62@users.noreply.github.com>
2022-09-05 09:28:44 +0000

    [Add] チェックシート項目28

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body)
   end
 
 end
