8e1f83d719ab3f2ab28ac82aa796b38e26765fae
ginama62 <107463338+ginama62@users.noreply.github.com>
2022-09-11 10:37:23 +0000

    [Finish]課題3：アプリケーションを作成してみよう：応用編

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+before_action :ensure_correct_user, only:[:edit, :update, :destroy]
 
   def create
     @book = Book.new(book_params)
@@ -49,6 +50,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+   @book = Book.find(params[:id])
+    unless @book.user == current_user
+    redirect_to books_path
+    end
   end
 
 end
